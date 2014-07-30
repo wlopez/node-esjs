d568244a13ec3849b8290c199c4d75e54d8e488c
Will Lopez <wmlpez@gmail.com>
2014-07-30 15:38:32 -0500

    initial revision

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodejsElsasticsearchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nodejsElsasticsearchApp
+ */
+angular.module('nodejsElsasticsearchApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
